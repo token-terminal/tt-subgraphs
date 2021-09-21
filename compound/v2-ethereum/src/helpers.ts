89f165fdb82166013f80300df230a7dd0e1175d8
Robert Valta <robert.j.valta@gmail.com>
2021-09-21 23:12:14 +0300

    Initialize Markets entity

--- compound/v2-ethereum/src/helpers.ts
+++ compound/v2-ethereum/src/helpers.ts
@@ -1,5 +1,6 @@
 import { BigDecimal, BigInt, Address } from "@graphprotocol/graph-ts";
 import { ComptrollerImplementation, Market } from "../generated/schema";
+import { ERC20 } from "../generated/templates/CToken/ERC20";
 
 export let ZERO_BD = BigDecimal.fromString("0");
 export let ZERO_BI = BigInt.fromString("0");
@@ -16,6 +17,27 @@ export function getOrCreateMarket(id: string): Market {
   let market = Market.load(id);
   if (market == null) {
       market = new Market(id);
+      market.totalFeesGenerated = ZERO_BD;
+      market.totalBorrows = ZERO_BD;
+      market.denomination = null;
   }
   return market as Market;
+}
+
+export function getMarket(id: string): Market {
+  return Market.load(id) as Market;
+}
+
+export function isMarket(id: string): boolean {
+  return getMarket(id) !== null;
+}
+
+export function amountToDenomination(amount: BigInt, decimals: i32): BigDecimal {
+  return amount.toBigDecimal().div(BigInt.fromI32(10).pow(decimals as u8).toBigDecimal());
+}
+
+export function getTokenDecimals(tokenAddress: Address): i32 {
+  let token = ERC20.bind(tokenAddress);
+  let result = token.try_decimals();
+  return result.reverted ? 0 : result.value;
 }
--- compound/v2-ethereum/src/mapping.ts
+++ compound/v2-ethereum/src/mapping.ts
@@ -2,12 +2,13 @@ import { Address } from "@graphprotocol/graph-ts";
 import {
   NewImplementation,
 } from "../generated/Comptroller/Comptroller";
-import {
-  ComptrollerImplementation as ComptrollerTemplate
-} from "../generated/templates";
-import { MarketListed } from "../generated/templates/ComptrollerImplementation/Comptroller"
+import { CToken as CTokenTemplate } from "../generated/templates";
+import { MarketListed } from "../generated/Comptroller/Comptroller";
+import { AccrueInterest } from "../generated/templates/CToken/CToken";
+import { CToken } from "../generated/templates/CToken/CToken";
 import { ComptrollerImplementationEvent } from "../generated/schema";
-import { getOrCreateComptrollerImplementation, getOrCreateMarket } from "./helpers";
+import { getOrCreateComptrollerImplementation, getOrCreateMarket, getMarket, isMarket, getTokenDecimals, amountToDenomination } from "./helpers";
+import { CETH_TOKEN_ADDRESS, WETH_TOKEN_ADDRESS } from "./constants";
 
 export function handleNewImplementation(event: NewImplementation): void {
   let newComptrollerImplementationEvent = new ComptrollerImplementationEvent(event.transaction.hash.toHexString());
@@ -17,8 +18,6 @@ export function handleNewImplementation(event: NewImplementation): void {
   newComptrollerImplementationEvent.newImplementation = newImplementation;
   newComptrollerImplementationEvent.save();
 
-  ComptrollerTemplate.create(newImplementation);
-
   let comptroller = getOrCreateComptrollerImplementation(newImplementation.toHexString());
   comptroller.save();
 }
@@ -26,6 +25,39 @@ export function handleNewImplementation(event: NewImplementation): void {
 export function handleMarketListed(event: MarketListed): void {
   let ctokenAddress = event.params.cToken;
 
-  let market = getOrCreateMarket(ctokenAddress.toHexString());
+  CTokenTemplate.create(ctokenAddress);
+
+  let ctoken = CToken.bind(ctokenAddress);
+
+  let tryDenominaton = ctoken.try_underlying();
+  let market = getOrCreateMarket(ctokenAddress.toHexString());  
+
+  if (ctokenAddress == Address.fromString(CETH_TOKEN_ADDRESS)) {
+    market.denomination = Address.fromString(WETH_TOKEN_ADDRESS);
+  } else {
+    if (!tryDenominaton.reverted) {
+      market.denomination = tryDenominaton.value;
+    }
+  }
+  market.save();
+}
+
+export function handleAccrueInterest(event: AccrueInterest): void {
+  let interestAccumulated = event.params.interestAccumulated;
+  let totalBorrows = event.params.totalBorrows;
+  let marketAddress = event.address.toHexString()
+
+  if (!isMarket(marketAddress)) {
+    return;
+  }
+
+  let market = getMarket(marketAddress);
+
+  let underlyingDecimals = getTokenDecimals(market.denomination as Address);
+  let feesGenerated = amountToDenomination(interestAccumulated, underlyingDecimals);
+  let borrows = amountToDenomination(totalBorrows, underlyingDecimals);
+
+  market.totalFeesGenerated = market.totalFeesGenerated.plus(feesGenerated);
+  market.totalBorrows = borrows;
   market.save();
 }
