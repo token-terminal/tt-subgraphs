2cfc3ee82ed209aee05b29564d3fa9f75dc9014a
fnery <fabiornery@gmail.com>
2021-11-10 11:16:57 +0000

    feat: add support for fees denomination

--- ens/v1-ethereum/src/mapping.ts
+++ ens/v1-ethereum/src/mapping.ts
@@ -1,24 +1,36 @@
 import {
   NameRegistered,
-  NameRenewed
-} from "../generated/EthRegistrarController/EthRegistrarController"
-import { getOrCreateGlobal, exponentToBigDecimal } from "./helpers";
-import { WEI_DECIMALS } from "./constants";
+  NameRenewed,
+} from "../generated/EthRegistrarController/EthRegistrarController";
+import {
+  getOrCreateGlobal,
+  getOrCreateToken,
+  exponentToBigDecimal,
+} from "./helpers";
+import { WEI_DECIMALS, WETH_TOKEN_ADDRESS } from "./constants";
 
 export function handleNameRegistered(event: NameRegistered): void {
-  let global = getOrCreateGlobal();
+  let token = getOrCreateToken(WETH_TOKEN_ADDRESS);
+  token.save();
+  let global = getOrCreateGlobal(token);
   let registrationCost = event.params.cost;
 
-  global.registrationFees = global.registrationFees
-    .plus(registrationCost.toBigDecimal().div(exponentToBigDecimal(WEI_DECIMALS)));
+  global.registrationFees = global.registrationFees.plus(
+    registrationCost.toBigDecimal().div(exponentToBigDecimal(WEI_DECIMALS))
+  );
+  global.denomination = token.id;
   global.save();
 }
 
 export function handleNameRenewed(event: NameRenewed): void {
-  let global = getOrCreateGlobal();
+  let token = getOrCreateToken(WETH_TOKEN_ADDRESS);
+  token.save();
+  let global = getOrCreateGlobal(token);
   let renewalCost = event.params.cost;
 
-  global.renewalFees = global.renewalFees
-    .plus(renewalCost.toBigDecimal().div(exponentToBigDecimal(WEI_DECIMALS)));
+  global.renewalFees = global.renewalFees.plus(
+    renewalCost.toBigDecimal().div(exponentToBigDecimal(WEI_DECIMALS))
+  );
+  global.denomination = token.id;
   global.save();
 }
